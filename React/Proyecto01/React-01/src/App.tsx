a6b43e40c78ae9ad9f29b5c4cfbb428456377981
Rafa-77 <alex_brandi77@hotmail.com>
2024-06-20 00:46:26 -0600

    modified:   React/Proyecto01/React-01/src/App.tsx 	new file:   React/Proyecto01/React-01/src/components/Alert.tsx 	new file:   React/Proyecto01/React-01/src/components/Button.tsx 	modified:   React/Proyecto01/React-01/src/components/ListGroup.tsx

--- React/Proyecto01/React-01/src/App.tsx
+++ React/Proyecto01/React-01/src/App.tsx
@@ -1,7 +1,36 @@
-import ListGroup from "./components/ListGroup";
+// ListGroup
 
-function App(){
-  return <div><ListGroup/></div>;
+// import ListGroup from "./components/ListGroup";
+
+// function App() {
+//   let items = ["New York", "San Francisco", "Tokio", "Paris"];
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading={"Cities"}
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Button
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button color="secondary" onClick={() => console.log("Clicled")}>
+        Label
+      </Button>
+    </div>
+  );
 }
 
 export default App;
