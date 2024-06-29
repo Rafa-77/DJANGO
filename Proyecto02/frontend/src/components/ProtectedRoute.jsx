e1f7bbb3e368bd3c499ebd18f23d822374c01919
Rafa-77 <alex_brandi77@hotmail.com>
2024-06-29 00:21:49 -0600

    modified:   Proyecto02/frontend/src/App.jsx 	modified:   Proyecto02/frontend/src/components/ProtectedRoute.jsx 	new file:   Proyecto02/frontend/src/pages/Home.jsx 	new file:   Proyecto02/frontend/src/pages/Login.jsx 	new file:   Proyecto02/frontend/src/pages/NotFound.jsx 	new file:   Proyecto02/frontend/src/pages/Register.jsx

--- Proyecto02/frontend/src/components/ProtectedRoute.jsx
+++ Proyecto02/frontend/src/components/ProtectedRoute.jsx
@@ -1,31 +1,58 @@
-import { Natigate } from "react-router-dom";
-import { jwtDecote } from "jwt-decode";
+import { Navigate, useFetcher } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function ProtectedRoute({ children }) {
-  const { isAuthorized, setIsAuthoried } = useState(null);
-  const refreshToken = async () => {};
+  const { isAuthorized, setIsAuthorized } = useState(null);
+
+  // 1. Llamamos a auth()
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
+
+  const refreshToken = async () => {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = await api.post("/api/token/refresh/", {
+        refresh: refreshToken,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
+    }
+  };
+
   const auth = async () => {
+    // 2. auth() vera si tenemos un token
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (!token) {
-      setIsAuthoried(false);
+      setIsAuthorized(false);
       return;
     }
+    // 3. si tenemos un token y no esta expirado, ponemos setIsAuthorized = true
     const decoded = jwtDecode(token);
     const tokenExpiration = decoded.exp;
     const now = Date.now() / 1000;
     if (tokenExpiration < now) {
+      // 4. si esta expirado llamamos a refreshToken
       await refreshToken();
     } else {
-      setIsAuthoried(true);
+      setIsAuthorized(true);
     }
   };
   if (isAuthorized === null) {
     return <div>Loading...</div>;
   }
-  return isAuthorized ? children : <Natigate to="/login" />;
+  // 5. si a pesar de haber llamado a refresh token no esta autorizado regresamos a login
+  return isAuthorized ? children : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
